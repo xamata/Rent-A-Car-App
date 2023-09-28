6a62b2b3c0331dfbff6b19932795026ac03d14bb
Maximillian Mata <61128570+xamata@users.noreply.github.com>
2023-09-27 22:12:47 -0700

    SqlDataAccess and SqlData Created
    No code has been added to SqlData, this is where the data logic will occur.

---
+++ RentACarAppLibrary/Databases/SqlDataAccess.cs
@@ -0,0 +1,54 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace RentACarAppLibrary.DataAccess
+{
+    public class SqlDataAccess : ISqlDataAccess
+    {
+        private readonly IConfiguration _config;
+
+        public SqlDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+        public List<T> LoadData<T, U>(string sqlStatement,
+                                     U parameter,
+                                     string connectionStringName,
+                                     bool isStoredProcedure = false)
+        {
+            string connectionString = _config.GetConnectionString(connectionStringName);
+            CommandType commandType = CommandType.Text;
+
+            if (isStoredProcedure == true)
+            {
+                commandType = CommandType.StoredProcedure;
+            }
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                List<T> rows = connection.Query<T>(sqlStatement, parameter, commandType: commandType).ToList();
+                return rows;
+            }
+        }
+        public void SaveData<T>(string sqlStatement,
+                                T parameter,
+                                string connectionStringName,
+                                bool isStoredProcedure = false)
+        {
+            string connectionString = _config.GetConnectionString(connectionStringName);
+            CommandType commandType = CommandType.Text;
+
+            if (isStoredProcedure == true)
+            {
+                commandType = CommandType.StoredProcedure;
+            }
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Execute(sqlStatement, parameter, commandType: commandType);
+            }
+        }
+    }
+}
