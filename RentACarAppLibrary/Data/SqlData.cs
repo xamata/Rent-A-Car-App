4da1200ace75b194f3a7469b4f3af13026b92b6e
Maximillian Mata <61128570+xamata@users.noreply.github.com>
2023-09-28 23:51:23 -0700

    Connection Between SqlData and ISqlDataAccess Created
    SqlData constructor initializes ISqlDataAccess

--- RentACarAppLibrary/Data/SqlData.cs
+++ RentACarAppLibrary/Data/SqlData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RentACarAppLibrary.DataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,11 @@ namespace RentACarAppLibrary.Data
 {
     public class SqlData
     {
+        private readonly ISqlDataAccess _db;
+
+        public SqlData(ISqlDataAccess db)
+        {
+            _db = db;
+        }
     }
 }
