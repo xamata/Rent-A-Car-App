262554140ceee3b6d1f668a029bb981ee21337b3
Maximillian Mata <61128570+xamata@users.noreply.github.com>
2023-09-29 18:28:21 -0700

    Car Search Page Created
    This page contains the search for available cars.

---
+++ RentACarApp.Web/Pages/CarSearch.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RentACarApp.Web.Pages
+{
+    public class CarSearchModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
